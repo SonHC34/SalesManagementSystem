7578404f02847f22fdae6fcb50c0a0b7da7ef743
SonHC <hoangcongson777@gmail.com>
2026-05-25 14:49:10 +0700

    first commit

---
+++ SalesManagementSystem/src/model/Product.java
@@ -0,0 +1,31 @@
+package model;
+
+/*
+Các quy tắc đặt tên:
+Fields: 
+- idProduct: int
+- nameProduct: String
+- categoryProduct: String
+- price: double
+- stockQuantity: int
+Method:  
++ updateStockQuantity(): void 
+*/
+
+public class Product {
+    private int idProduct;
+    private String nameProduct;
+    private String categoryProduct; 
+    private double price;
+    private int stockQuantity;
+
+    // Constructor
+    public Product() {
+        
+    }
+
+    public void UpdateStockProduct() {
+
+    }
+}
+
